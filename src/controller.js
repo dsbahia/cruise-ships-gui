385e76d9cc8d95eb7f5c8d048b7ab2ec2b2cf8a3
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-06-07 21:15:38 +0100

    Added setSail method within controller constructor and modified some styling elements, added setsail button to index.html

--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,14 @@
 (function exportController() {
   class Controller {
-    constructor() {
+    constructor(ship) {
       this.counter = 0;
       this.initializeSea();
+      this.ship = ship;
+
+      const sailButton = document.querySelector("#sailbutton");
+      sailButton.addEventListener("click", () => {
+        this.setSail();
+      });
     }
 
     initializeSea() {
@@ -31,17 +37,51 @@
       portsElement.style.width = `${portCount * portWidth}px`;
     }
 
-    renderShip(ship) {
-        const portElement = document.querySelector(`[data-port-index="${ship.itinerary.ports.indexOf(ship.currentPort)}"]`);
-        const shipElement = document.querySelector('#ship');
-        const offsetAdjustment = 32;
-        
-        const shipTop = portElement.offsetTop + offsetAdjustment;
-        const shipLeft = portElement.offsetLeft - offsetAdjustment;
-        
-        shipElement.style.top = `${shipTop}px`;
-        shipElement.style.left = `${shipLeft}px`;
-    };
+    renderShip() {
+      const portElement = document.querySelector(
+        `[data-port-index="${this.ship.itinerary.ports.indexOf(
+          this.ship.currentPort
+        )}"]`
+      );
+      const shipElement = document.querySelector("#ship");
+      const offsetAdjustment = 32;
+
+      const shipTop = portElement.offsetTop + offsetAdjustment;
+      const shipLeft = portElement.offsetLeft - offsetAdjustment;
+
+      shipElement.style.top = `${shipTop}px`;
+      shipElement.style.left = `${shipLeft}px`;
+    }
+
+    setSail() {
+      const ship = this.ship;
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+    
+      if (!nextPortElement) {
+        console.log("End of the route!");
+        return alert("End of the route!");
+      }
+    
+      const shipElement = document.querySelector("#ship");
+      const step = 1;
+      const interval = 20;
+    
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        const destinationLeft = nextPortElement.offsetLeft - 32;
+    
+        if (shipLeft === destinationLeft) {
+          clearInterval(sailInterval);
+          ship.setSail();
+          ship.dock();
+          console.log(`Reached port: ${ship.currentPort.name}`);
+        } else {
+          shipElement.style.left = `${shipLeft + step}px`;
+        }
+      }, interval);
+    }
   }
   if (typeof module !== "undefined" && module.exports) {
     module.exports = Controller;
